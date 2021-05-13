ffe776db5e931edb2dad63159948eab7ca28b955
Sangheon Lee <wwdbsh@gmail.com>
2021-05-13 14:41:54 -0400

    20210513:easy 121

---
+++ easy/121/121.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n < 2){
+            return 0;
+        }
+        int min_price = 1e9;
+        int max_profit = 0;
+        for(int i = 0; i < prices.size(); i++){
+            min_price = min(min_price, prices[i]);
+            max_profit = max(max_profit, prices[i]-min_price);
+        }
+        return max_profit;
+    }
+};
---
+++ easy/121/README.md
@@ -0,0 +1,4 @@
+## Best Time to Buy and Sell Stock
+
+* Time complexity : O(n). Only a single pass is needed.
+* Space complexity : O(1). Only two variables are used.
