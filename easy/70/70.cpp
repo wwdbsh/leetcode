bf740ac77b5bb48ce9ee41c6404f1f96ac56321c
Sangheon Lee <wwdbsh@gmail.com>
2021-06-20 19:00:04 -0700

    20210620:easy 70

---
+++ easy/70/70.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[46];
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
---
+++ easy/70/README.md
@@ -0,0 +1,5 @@
+## Climbing Stairs
+
+* dp
+* Time complexity : O(n). Single loop upto n.
+* Space complexity : O(n). dpdp array of size n is used.
